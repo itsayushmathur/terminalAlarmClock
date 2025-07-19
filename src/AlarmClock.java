f8b7e9d6eadb3d621868561bc68a43060b487c28
Ayush Mathur <ayush.mathur@incture.com>
2025-07-19 18:31:22 +0530

    Working alarm

---
+++ src/AlarmClock.java
@@ -0,0 +1,62 @@
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalTime;
+import java.util.Scanner;
+
+public class AlarmClock implements Runnable {
+
+    private final LocalTime alarmTime;
+    private final String filePath;
+    private final Scanner scanner;
+
+    AlarmClock(LocalTime alarmTime, String filePath, Scanner scanner) {
+        this.alarmTime = alarmTime;
+        this.filePath = filePath;
+        this.scanner=scanner;
+    }
+
+    @Override
+    public void run() {
+
+
+        while (LocalTime.now().isBefore(alarmTime)) {
+            try {
+                Thread.sleep(1000);
+                LocalTime now = LocalTime.now();
+
+                System.out.printf("\r%02d:%02d:%02d", now.getHour(), now.getMinute(), now.getSecond());
+
+            } catch (InterruptedException e) {
+                System.out.println("Thread Interrupted");
+            }
+        }
+        System.out.println("Hey! Time's Up");
+        playSound(filePath);
+    }
+
+    private void playSound(String filePath) {
+
+        File audioFile = new File(filePath);
+
+        try (AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);) {
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+            System.out.print("Enter to stop alarm: ");
+            scanner.nextLine();
+            clip.stop();
+
+            scanner.close();
+
+//            Thread.sleep(5000);
+        } catch (UnsupportedAudioFileException e) {
+            System.out.println("Audio file format not Supported");
+        } catch (IOException e) {
+            System.out.println("Error reading audio file");
+        } catch (LineUnavailableException e) {
+            System.out.println("Audio is Unavailable");
+        }
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,34 @@
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner=new Scanner(System.in);
-        DateTimeFormatter formatter= DateTimeFormatter.ofPattern("HH:mm:ss");
-        scanner.close();
+        Scanner scanner = new Scanner(System.in);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalTime alarmTime = null;
+        String filePath="Cash - Mark Karan, Scott Guberman, Angeline Saris, Jeremy Hoenig.wav";
+
+        while (alarmTime == null) {
+            try {
+                System.out.print("Enter an alarm time (HH:MM:SS): ");
+                String inputTime = scanner.nextLine();
+
+                alarmTime = LocalTime.parse(inputTime, formatter);
+                System.out.println("Alarm set for " + alarmTime);
+            } catch (DateTimeParseException e) {
+                System.out.println("Invalid Format.Please use HH:MM:SS");
+            }
+        }
+
+        AlarmClock alarmClock = new AlarmClock(alarmTime, filePath,scanner);
+        Thread alarmThread = new Thread(alarmClock);
+        alarmThread.start();
+
+
+
 
 
     }
