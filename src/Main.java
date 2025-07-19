293ea7d8d04b8b6305c9a6f5d4db5d7d3ec93d06
Ayush Mathur <ayush.mathur@incture.com>
2025-07-19 14:31:26 +0530

    .

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner=new Scanner(System.in);
+        DateTimeFormatter formatter= DateTimeFormatter.ofPattern("HH:mm:ss");
+        scanner.close();
 
 
     }
